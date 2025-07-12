6a4906c7d982f16d0df00a040b91c7cc0cf85592
panxiao_mindslab <panxiao@wustl.edu>
2025-07-11 20:52:18 -0500

    uploading text_gcn code

---
+++ code/Impression/text_gcn/build_graph_impression.py
@@ -0,0 +1,381 @@
+import os
+import random
+import numpy as np
+import pickle as pkl
+import networkx as nx
+import scipy.sparse as sp
+from utils import loadWord2Vec, clean_str
+from math import log
+import sys
+from scipy.spatial.distance import cosine
+
+if len(sys.argv) != 2:
+	sys.exit("Use: python build_graph_impression.py <dataset>")
+
+datasets = ['rad']
+# build corpus
+dataset = sys.argv[1]
+
+if dataset not in datasets:
+	sys.exit("wrong dataset name")
+
+
+word_embeddings_dim = 300
+word_vector_map = {}   #https://github.com/yao8839836/text_gcn/issues/90
+
+#shuffle_doc_words_str;   load train.clean.txt; validation.clean.txt; test.clean.txt
+#train data and label
+with open("./Impression/text_gcn/train.clean.txt") as trainc:
+    Lines_train = trainc.readlines()
+label_train = np.load("./Impression/text_gcn/train.label.npy")
+
+#validation data and label
+with open("./Impression/text_gcn/validation.clean.txt") as vc:
+    Lines_validation = vc.readlines()
+label_validation = np.load("./Impression/text_gcn/validation.label.npy")
+
+#test data and label
+with open("./Impression/text_gcn/test.clean.txt") as testc:
+    Lines_test = testc.readlines()
+label_test = np.load("./Impression/text_gcn/test.label.npy")
+
+shuffle_doc_words_list = Lines_train + Lines_validation + Lines_test
+labels = np.concatenate((label_train, label_validation, label_test)).tolist()
+
+# label list
+label_set = set()
+for label in labels:
+    label_set.add(label)
+label_list = list(label_set)
+
+# build vocab
+word_freq = {}
+word_set = set()
+for doc_words in shuffle_doc_words_list:
+    words = doc_words.split()
+    for word in words:
+        word_set.add(word)
+        if word in word_freq:
+            word_freq[word] += 1
+        else:
+            word_freq[word] = 1
+
+vocab = list(word_set)
+vocab_size = len(vocab)
+
+word_doc_list = {}
+
+for i in range(len(shuffle_doc_words_list)):
+    doc_words = shuffle_doc_words_list[i]
+    words = doc_words.split()
+    appeared = set()
+    for word in words:
+        if word in appeared:
+            continue
+        if word in word_doc_list:
+            doc_list = word_doc_list[word]
+            doc_list.append(i)
+            word_doc_list[word] = doc_list
+        else:
+            word_doc_list[word] = [i]
+        appeared.add(word)
+
+word_doc_freq = {}
+for word, doc_list in word_doc_list.items():
+    word_doc_freq[word] = len(doc_list)
+
+word_id_map = {}
+for i in range(vocab_size):
+    word_id_map[vocab[i]] = i
+
+
+# x: feature vectors of training docs, no initial features
+# slect 90% training set
+real_train_size = len(label_train)
+val_size = len(label_validation)
+train_size = real_train_size + val_size  # - int(0.5 * train_size)
+# different training rates
+
+row_x = []
+col_x = []
+data_x = []
+for i in range(real_train_size):
+    doc_vec = np.array([0.0 for k in range(word_embeddings_dim)])
+    doc_words = shuffle_doc_words_list[i]
+    words = doc_words.split()
+    doc_len = len(words)
+    for word in words:
+        if word in word_vector_map:
+            word_vector = word_vector_map[word]
+            doc_vec = doc_vec + np.array(word_vector)
+
+    for j in range(word_embeddings_dim):
+        row_x.append(i)
+        col_x.append(j)
+        data_x.append(doc_vec[j] / doc_len)  # doc_vec[j]/ doc_len
+
+# x = sp.csr_matrix((real_train_size, word_embeddings_dim), dtype=np.float32)
+x = sp.csr_matrix((data_x, (row_x, col_x)), shape=(
+    real_train_size, word_embeddings_dim))
+
+y = []
+for i in range(real_train_size):
+    #doc_meta = shuffle_doc_name_list[i]
+    #temp = doc_meta.split('\t')
+    label = label_train[i]
+    one_hot = [0 for l in range(len(label_list))]
+    label_index = label_list.index(label)
+    one_hot[label_index] = 1
+    y.append(one_hot)
+y = np.array(y)
+print(y)
+
+# tx: feature vectors of test docs, no initial features
+test_size = len(label_test)
+
+row_tx = []
+col_tx = []
+data_tx = []
+for i in range(test_size):
+    doc_vec = np.array([0.0 for k in range(word_embeddings_dim)])
+    doc_words = shuffle_doc_words_list[i + train_size]
+    words = doc_words.split()
+    doc_len = len(words)
+    for word in words:
+        if word in word_vector_map:
+            word_vector = word_vector_map[word]
+            doc_vec = doc_vec + np.array(word_vector)
+
+    for j in range(word_embeddings_dim):
+        row_tx.append(i)
+        col_tx.append(j)
+        # np.random.uniform(-0.25, 0.25)
+        data_tx.append(doc_vec[j] / doc_len)  # doc_vec[j] / doc_len
+
+# tx = sp.csr_matrix((test_size, word_embeddings_dim), dtype=np.float32)
+tx = sp.csr_matrix((data_tx, (row_tx, col_tx)),
+                   shape=(test_size, word_embeddings_dim))
+
+ty = []
+for i in range(test_size):
+    #doc_meta = shuffle_doc_name_list[i + train_size]
+    #temp = doc_meta.split('\t')
+    #label = temp[2]
+    label = label_test[i]
+    one_hot = [0 for l in range(len(label_list))]
+    label_index = label_list.index(label)
+    one_hot[label_index] = 1
+    ty.append(one_hot)
+ty = np.array(ty)
+print(ty)
+
+# allx: the the feature vectors of both labeled and unlabeled training instances
+# (a superset of x)
+# unlabeled training instances -> words
+
+word_vectors = np.random.uniform(-0.01, 0.01,
+                                 (vocab_size, word_embeddings_dim))
+
+for i in range(len(vocab)):
+    word = vocab[i]
+    if word in word_vector_map:
+        vector = word_vector_map[word]
+        word_vectors[i] = vector
+
+row_allx = []
+col_allx = []
+data_allx = []
+
+for i in range(train_size):
+    doc_vec = np.array([0.0 for k in range(word_embeddings_dim)])
+    doc_words = shuffle_doc_words_list[i]
+    words = doc_words.split()
+    doc_len = len(words)
+    for word in words:
+        if word in word_vector_map:
+            word_vector = word_vector_map[word]
+            doc_vec = doc_vec + np.array(word_vector)
+
+    for j in range(word_embeddings_dim):
+        row_allx.append(int(i))
+        col_allx.append(j)
+        # np.random.uniform(-0.25, 0.25)
+        data_allx.append(doc_vec[j] / doc_len)  # doc_vec[j]/doc_len
+for i in range(vocab_size):
+    for j in range(word_embeddings_dim):
+        row_allx.append(int(i + train_size))
+        col_allx.append(j)
+        data_allx.append(word_vectors.item((i, j)))
+
+
+row_allx = np.array(row_allx)
+col_allx = np.array(col_allx)
+data_allx = np.array(data_allx)
+
+allx = sp.csr_matrix(
+    (data_allx, (row_allx, col_allx)), shape=(train_size + vocab_size, word_embeddings_dim))
+
+ally = []
+for i in range(train_size):
+    #doc_meta = shuffle_doc_name_list[i]
+    #temp = doc_meta.split('\t')
+    #label = temp[2]
+    label = labels[i]
+    one_hot = [0 for l in range(len(label_list))]
+    label_index = label_list.index(label)
+    one_hot[label_index] = 1
+    ally.append(one_hot)
+
+for i in range(vocab_size):
+    one_hot = [0 for l in range(len(label_list))]
+    ally.append(one_hot)
+
+ally = np.array(ally)
+
+print(x.shape, y.shape, tx.shape, ty.shape, allx.shape, ally.shape)
+
+'''
+Doc word heterogeneous graph
+'''
+
+# word co-occurence with context windows
+window_size = 20
+windows = []
+
+for doc_words in shuffle_doc_words_list:
+    words = doc_words.split()
+    length = len(words)
+    if length <= window_size:
+        windows.append(words)
+    else:
+        # print(length, length - window_size + 1)
+        for j in range(length - window_size + 1):
+            window = words[j: j + window_size]
+            windows.append(window)
+            # print(window)
+
+
+word_window_freq = {}
+for window in windows:
+    appeared = set()
+    for i in range(len(window)):
+        if window[i] in appeared:
+            continue
+        if window[i] in word_window_freq:
+            word_window_freq[window[i]] += 1
+        else:
+            word_window_freq[window[i]] = 1
+        appeared.add(window[i])
+
+word_pair_count = {}
+for window in windows:
+    for i in range(1, len(window)):
+        for j in range(0, i):
+            word_i = window[i]
+            word_i_id = word_id_map[word_i]
+            word_j = window[j]
+            word_j_id = word_id_map[word_j]
+            if word_i_id == word_j_id:
+                continue
+            word_pair_str = str(word_i_id) + ',' + str(word_j_id)
+            if word_pair_str in word_pair_count:
+                word_pair_count[word_pair_str] += 1
+            else:
+                word_pair_count[word_pair_str] = 1
+            # two orders
+            word_pair_str = str(word_j_id) + ',' + str(word_i_id)
+            if word_pair_str in word_pair_count:
+                word_pair_count[word_pair_str] += 1
+            else:
+                word_pair_count[word_pair_str] = 1
+
+row = []
+col = []
+weight = []
+
+# pmi as weights
+
+num_window = len(windows)
+
+for key in word_pair_count:
+    temp = key.split(',')
+    i = int(temp[0])
+    j = int(temp[1])
+    count = word_pair_count[key]
+    word_freq_i = word_window_freq[vocab[i]]
+    word_freq_j = word_window_freq[vocab[j]]
+    pmi = log((1.0 * count / num_window) /
+              (1.0 * word_freq_i * word_freq_j/(num_window * num_window)))
+    if pmi <= 0:
+        continue
+    row.append(train_size + i)
+    col.append(train_size + j)
+    weight.append(pmi)
+
+
+# doc word frequency
+doc_word_freq = {}
+
+for doc_id in range(len(shuffle_doc_words_list)):
+    doc_words = shuffle_doc_words_list[doc_id]
+    words = doc_words.split()
+    for word in words:
+        word_id = word_id_map[word]
+        doc_word_str = str(doc_id) + ',' + str(word_id)
+        if doc_word_str in doc_word_freq:
+            doc_word_freq[doc_word_str] += 1
+        else:
+            doc_word_freq[doc_word_str] = 1
+
+for i in range(len(shuffle_doc_words_list)):
+    doc_words = shuffle_doc_words_list[i]
+    words = doc_words.split()
+    doc_word_set = set()
+    for word in words:
+        if word in doc_word_set:
+            continue
+        j = word_id_map[word]
+        key = str(i) + ',' + str(j)
+        freq = doc_word_freq[key]
+        if i < train_size:
+            row.append(i)
+        else:
+            row.append(i + vocab_size)
+        col.append(train_size + j)
+        idf = log(1.0 * len(shuffle_doc_words_list) /
+                  word_doc_freq[vocab[j]])
+        weight.append(freq * idf)
+        doc_word_set.add(word)
+
+node_size = train_size + vocab_size + test_size
+adj = sp.csr_matrix(
+    (weight, (row, col)), shape=(node_size, node_size))
+
+# dump objects
+f = open("./Impression/text_gcn/ind.{}.x".format(dataset), 'wb')
+pkl.dump(x, f)
+f.close()
+
+f = open("./Impression/text_gcn/ind.{}.y".format(dataset), 'wb')
+pkl.dump(y, f)
+f.close()
+
+f = open("./Impression/text_gcn/ind.{}.tx".format(dataset), 'wb')
+pkl.dump(tx, f)
+f.close()
+
+f = open("./Impression/text_gcn/ind.{}.ty".format(dataset), 'wb')
+pkl.dump(ty, f)
+f.close()
+
+f = open("./Impression/text_gcn/ind.{}.allx".format(dataset), 'wb')
+pkl.dump(allx, f)
+f.close()
+
+f = open("./Impression/text_gcn/ind.{}.ally".format(dataset), 'wb')
+pkl.dump(ally, f)
+f.close()
+
+f = open("./Impression/text_gcn/ind.{}.adj".format(dataset), 'wb')
+pkl.dump(adj, f)
+f.close()
---
+++ code/Impression/text_gcn/remove_words_rad_impression.py
@@ -0,0 +1,147 @@
+#environment: textgcn
+from nltk.corpus import stopwords
+import nltk
+from nltk.wsd import lesk
+from nltk.corpus import wordnet as wn
+from utils import clean_str, loadWord2Vec
+import sys
+import pandas as pd
+from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import Word2Vec
+import numpy as np
+import random
+
+if len(sys.argv) != 2:
+	sys.exit("Use: python remove_words_rad_impression.py <dataset>")
+
+datasets = ['rad']
+dataset = sys.argv[1]
+
+if dataset not in datasets:
+	sys.exit("wrong dataset name")
+
+#read train set
+train_list = open("./Impression/Train/train.txt",'r')
+train_data = []; train_label = []
+for line in train_list:
+    label, text = line.split('\t')
+    train_data.append(text)
+    if label == 'followup':
+        train_label.append(1)
+    elif label == 'nofollowup':
+        train_label.append(0)
+train_list.close()
+c = list(zip(train_data, train_label)); random.shuffle(c); train_data, train_label = zip(*c)
+#df_train = pd.DataFrame(train_data, columns = ['Report Text'])
+
+#read validation set
+validation_list = open("./Impression/Validation/validation.txt",'r')
+validation_data = []; validation_label = []
+for line in validation_list:
+    label, text = line.split('\t')
+    validation_data.append(text)
+    if label == 'followup':
+        validation_label.append(1)
+    elif label == 'nofollowup':
+        validation_label.append(0)
+validation_list.close()
+#df_validation = pd.DataFrame(validation_data, columns = ['Report Text'])
+
+#read test set
+test_list = open("./two/Impression/Test/test.txt",'r')
+test_data = []; test_label = []
+for line in test_list:
+    label, text = line.split('\t')
+    test_data.append(text)
+    if label == 'followup':
+        test_label.append(1)
+    elif label == 'nofollowup':
+        test_label.append(0)
+test_list.close()
+print('test_data:' + str(len(test_data)))
+
+word_freq = {}  # to remove rare words
+
+for doc_content in train_data:
+    words = doc_content.split()
+    for word in words:
+        if word in word_freq:
+            word_freq[word] += 1
+        else:
+            word_freq[word] = 1
+
+#remove low frequence words from the documentary
+clean_docs_train = []
+for doc_content_train in train_data:
+    #temp = clean_str(doc_content)
+    words = doc_content_train.split()
+    doc_words = []
+    for word in words:
+        if word in word_freq and word_freq[word] >= 5:
+            doc_words.append(word)
+    doc_str = ' '.join(doc_words).strip()
+    clean_docs_train.append(doc_str)
+
+temp = list(zip(clean_docs_train, train_label))
+np.random.shuffle(temp)
+clean_docs_train, train_label = zip(*temp)
+clean_docs_train = list(clean_docs_train); train_label = np.array(train_label)
+np.save('./Impression/text_gcn/train.label', train_label)
+
+clean_corpus_str_train = '\n'.join(clean_docs_train) #use \n to connect each document.
+
+#save cleaned corpus to file
+f = open('./Impression/text_gcn/train.clean.txt', 'w')
+f.write(clean_corpus_str_train)
+f.close()
+
+print('-------------------------------------------------------------------')
+
+#remove low frequence words from the documentary
+clean_docs_validation = []
+for doc_content_validation in validation_data:
+    #temp = clean_str(doc_content)
+    words = doc_content_validation.split()
+    doc_words = []
+    for word in words:
+        if word in word_freq and word_freq[word] >= 5:
+            doc_words.append(word)
+    doc_str = ' '.join(doc_words).strip()
+    clean_docs_validation.append(doc_str)
+
+clean_docs_validation = list(clean_docs_validation); validation_label = np.array(validation_label)
+np.save('./Impression/text_gcn/validation.label', validation_label)
+
+clean_corpus_str_validation = '\n'.join(clean_docs_validation) #use \n to connect each document.
+
+#save cleaned corpus to file
+f = open('./Impression/text_gcn/validation.clean.txt', 'w')
+f.write(clean_corpus_str_validation)
+f.close()
+
+clean_docs_test = []
+for doc_content_test in test_data:
+    #temp = clean_str(doc_content)
+    words = doc_content_test.split()
+    doc_words = []
+    for word in words:
+        if word in word_freq and word_freq[word] >= 5:
+            doc_words.append(word)
+        #else:
+        #    print(word)
+    doc_str = ' '.join(doc_words).strip()
+    clean_docs_test.append(doc_str)
+
+
+clean_docs_test = list(clean_docs_test); test_label = np.array(test_label)
+np.save('./Impression/text_gcn/test.label', test_label)
+
+clean_corpus_str_test = '\n'.join(clean_docs_test) #use \n to connect each document.
+
+#save cleaned corpus to file
+f = open('./Impression/text_gcn/test.clean.txt', 'w')
+f.write(clean_corpus_str_test)
+f.close()
+
+      
+      
